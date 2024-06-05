89266708916bf8c17fc446344e6efb4a6d616b25
Naimur Rahman Nishat <139154659+NaimurRahmanNishat@users.noreply.github.com>
2024-06-05 13:56:28 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let x = [];
+let y = [];
+console.log(x==y);
+console.log(x===y);
+
+// NOTE : Loose Equality(==) This is check value and type.
+// NOTE : Strict Equality(===) This is check the value.
